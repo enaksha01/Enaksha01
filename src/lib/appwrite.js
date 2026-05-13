75b87e08c96f4d5234bcd3fe30f33061de5ce2e0
enaksha01 <enaksha01@gmail.com>
2026-05-13 14:10:57 +0530

    appwrite.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -10,7 +10,7 @@ export const storage = new Storage(client);
 
 // Aapki Final IDs yahan set hain
 export const APPWRITE_CONFIG = {
-    dbId: '6a0417e1002b225c119c', // Jo aapke dashboard pe naam/id dikh rahi hai
+    dbId: 'Enaksha01', // Jo aapke dashboard pe naam/id dikh rahi hai
     collectionId: '6a0421b1001d11330884', 
     bucketId: '6a0426ed002f7e8a2cf2' 
 };
