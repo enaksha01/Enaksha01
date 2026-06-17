c694be8f3b4fdb886df804c2568bd7ef85a9ffdf
enaksha01 <enaksha01@gmail.com>
2026-06-17 14:58:33 +0530

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Ye line zaroori hai dark mode memory ke liye
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandOrange: '#eb6923', // Logo Orange
+        brandBlue: '#4a77b3',   // Logo Blue
+      }
+    },
   },
   plugins: [],
 }
